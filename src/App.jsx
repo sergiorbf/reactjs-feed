3c2140c433cbd69378277a4f0fad791d4aa54337
sergiorbf <sbittencourt19@gmail.com>
2023-06-01 21:28:16 -0300

    iniciei iteracao nos posts, passando variaveis que representassem vir do banco de dados, como horarios, iconImg, texto no post, cargo, nome etc

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import { Sidebar } from './components/sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/sergiorbf.png',
+      name: 'Sergio Filho',
+      role: 'Junior Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2023-05-31 22:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/eryk-vieira.png',
+      name: 'Eryk Vieira',
+      role: 'Tech Lead'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare', },
+    ],
+    publishedAt: new Date('2023-05-25 20:00:00'),
+  }
+]
+
 export function App() {
 
   return (
@@ -13,14 +44,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Sergio Filho"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur totam dignissimos suscipit soluta atque id cum quos, ducimus voluptatem illo? Voluptas maiores hic perferendis ullam facere doloribus eum cumque assumenda."
-          />
-          <Post
-            author="Jonsons baby"
-            content=" bilu teteita"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
